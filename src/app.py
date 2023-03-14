630c5ac7fc516793d09b152087fe9cd525588334
E-Vivanco <vivanco.elsa@gmail.com>
2023-03-14 14:43:10 +0000

    ajustes 2°

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, jsonify,request, json
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
+
 todos = [
     { "label": "My first task", "done": False },
     { "label": "My second task", "done": False }
 ]
-@app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hello, World!'
+
 
 @app.route('/todos', methods=['GET'])
-def hello_world2():
+def hello_world():
     response = jsonify(todos)
     return response
 
@@ -27,5 +27,10 @@ def delete_todo(position):
     return jsonify(todos)
 
 
+
+
+# These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
+
+
